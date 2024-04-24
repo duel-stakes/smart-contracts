cc8c61e77e911c047a0b6fbb89e4435d4c4df56b
Gustavo Deps <gwdeps@gmail.com>
2024-04-24 19:07:44 -0300

    POC Version
    Version of POC
    Begin of tests
    No interface yet

---
+++ test/utils/mockERC20.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract mockERC20 is ERC20 {
+    constructor() ERC20("Payment","tkn"){
+        _mint(msg.sender, 10 ether);
+    }
+
+    function mint(uint256 _amount) external {
+        _mint(msg.sender, _amount);
+    }
+}
